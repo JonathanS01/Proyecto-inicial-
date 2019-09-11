32c6f6307eb5eebf9e2c07f7b9716a84f87d85ce
JonathanS01 <53585609+JonathanS01@users.noreply.github.com>
2019-09-10 21:52:00 -0500

    Add files via upload

--- Jona.java
+++ Jona.java
@@ -24,6 +24,7 @@ public class Jona {
         int a=10;
         int b=30;
         int n1,n2,n3,coco=18;
+        double x1,x2,x3;
         System.out.println(coco);
         System.out.println("La suma de "+a+" mas "+coco+" y "+b+" es: ");
         n1=a+b+coco;
@@ -37,6 +38,30 @@ public class Jona {
         n2=tecla.nextInt();
         n3=n1+n2;
         System.out.println("\nEl resultado de la suma es: "+n3);
+        System.out.println("\t\t\tVamos a realizar una resta");
+        Scanner resta=new Scanner(System.in);           
+        System.out.println("Ingrese primer numero");
+        n1=tecla.nextInt();
+        System.out.println("\nIngrese el segundo numero");
+        n2=tecla.nextInt();
+        n3=n1-n2;
+        System.out.println("\nEl resultado de la resta es: "+n3);
+        System.out.println("\t\t\tVamos a realizar una multiplicacion");
+        Scanner mult=new Scanner (System.in);                
+        System.out.println("Ingrese primer numero");
+        n1=tecla.nextInt();
+        System.out.println("\nIngrese el segundo numero");
+        n2=tecla.nextInt();
+        n3=n1*n2;
+        System.out.println("\nEl resultado de la multiplicacion es: "+n3);
+        System.out.println("\t\t\tVamos a realizar una division");
+        Scanner divi=new Scanner (System.in);                
+        System.out.println("Ingrese primer numero");
+        x1=tecla.nextInt();
+        System.out.println("\nIngrese el segundo numero");
+        x2=tecla.nextInt();
+        x3=x1/x2;
+        System.out.println("\nEl resultado de la suma es: "+x3);
     }
     
 }
